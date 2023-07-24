6068a3114aedcc27ad5e63b145dee5beb75d2f3a
ethanwater <angelshatepop@gmail.com>
2023-07-24 10:09:03 -0400

    rodio+mp3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::BufReader;
+const MUSIC_DIR: &str = "/Users/ethan/Music/Music/Media.localized/Music";
+
 fn main() {
-    println!("Hello, world!");
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let file = std::fs::File::open("/Users/ethan/Music/Music/Media.localized/Music/Nuffbonsai/Nuffbonsai Mixes/01 Classic Full Cycle.mp3").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+
+    println!("now playing: ");
+    sink.sleep_until_end();
 }
