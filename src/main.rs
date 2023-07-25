5ab58a876f337ace255ae981be309c4132abcb98
ethanwater <angelshatepop@gmail.com>
2023-07-25 15:58:32 -0400

    playlisterr

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,58 @@
+use std::fs;
 use std::io::BufReader;
-const MUSIC_DIR: &str = "/Users/ethan/Music/Music/Media.localized/Music";
+use std::path::{Path, PathBuf};
 
-fn main() {
-    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = rodio::Sink::try_new(&handle).unwrap();
+trait Mp3 {
+    fn is_mp3(&self) -> Option<()>;
+}
 
-    let file = std::fs::File::open("/Users/ethan/Music/Music/Media.localized/Music/Nuffbonsai/Nuffbonsai Mixes/01 Classic Full Cycle.mp3").unwrap();
-    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+impl Mp3 for Path {
+    fn is_mp3(&self) -> Option<()> {
+        let path = self.file_name()?.to_str()?;
+        if path.ends_with(".mp3") {
+            return Some(());
+        } else {
+            return None;
+        }
+    }
+}
 
-    println!("now playing: ");
-    sink.sleep_until_end();
+fn playlist<R>(root: R) -> std::io::Result<PathBuf>
+where
+    R: AsRef<Path>,
+{
+    let mut playlist = PathBuf::new();
+    for entry in fs::read_dir(&root)? {
+        let entry = entry?.path();
+        let path = entry.as_path();
+        if path.is_mp3().is_some() {
+            playlist.push(path);
+            dbg!(path);
+        }
+    }
+
+    Ok(playlist)
+}
+
+fn play<P>(playlist: P)
+where
+    P: AsRef<PathBuf> + std::iter::Iterator,
+    <P as Iterator>::Item: AsRef<Path>,
+{
+    for audio in playlist {
+        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        let sink = rodio::Sink::try_new(&handle).unwrap();
+
+        let file = std::fs::File::open(audio).unwrap();
+        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+
+        println!("now playing: ");
+        sink.sleep_until_end();
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let x = Path::new("/Users/ethan/Music/LiquidDNB");
+    let playlist = playlist(x);
+    play(playlist)?;
 }
