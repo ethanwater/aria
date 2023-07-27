b116937d184e51100d0f8155050a86403ae0a19e
ethanwater <angelshatepop@gmail.com>
2023-07-27 11:57:04 -0400

    yeah, i think i fucked up here

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::io::BufReader;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 trait Mp3 {
     fn is_mp3(&self) -> Option<()>;
@@ -17,43 +16,43 @@ impl Mp3 for Path {
     }
 }
 
-fn playlist<R>(root: R) -> std::io::Result<PathBuf>
+fn playlist<R>(root: R) -> std::io::Result<()>
 where
     R: AsRef<Path>,
 {
-    let mut playlist = PathBuf::new();
-    for entry in fs::read_dir(&root)? {
+    let mut playlist = Vec::new();
+    for entry in fs::read_dir(root)? {
         let entry = entry?.path();
-        let path = entry.as_path();
+        let path = entry.clone().as_path();
         if path.is_mp3().is_some() {
-            playlist.push(path);
+            playlist.push(path.to_str().unwrap());
+
             dbg!(path);
         }
     }
-
-    Ok(playlist)
+    dbg!(&playlist);
+    Ok(())
 }
 
-fn play<P>(playlist: P)
-where
-    P: AsRef<PathBuf> + std::iter::Iterator,
-    <P as Iterator>::Item: AsRef<Path>,
-{
-    for audio in playlist {
-        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-        let sink = rodio::Sink::try_new(&handle).unwrap();
-
-        let file = std::fs::File::open(audio).unwrap();
-        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-
-        println!("now playing: ");
-        sink.sleep_until_end();
-    }
-}
+//fn play<P>(playlist: P)
+//where
+//    P: AsRef<PathBuf> + std::iter::Iterator,
+//    <P as Iterator>::Item: AsRef<Path>,
+//{
+//    for audio in playlist {
+//        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+//        let sink = rodio::Sink::try_new(&handle).unwrap();
+//
+//        let file = std::fs::File::open(audio).unwrap();
+//        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+//
+//        println!("now playing: ");
+//        sink.sleep_until_end();
+//    }
+//}
 
 fn main() -> std::io::Result<()> {
     let x = Path::new("/Users/ethan/Music/LiquidDNB");
     let playlist = playlist(x);
-    play(playlist)?;
     Ok(())
 }
