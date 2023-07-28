59773250e315f5204134bfa568352d8d76c47333
ethanwater <angelshatepop@gmail.com>
2023-07-28 14:13:43 -0400

    it works!

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,38 @@
-use std::fs;
 use std::path::Path;
+use std::{fs, io};
 
-trait Mp3 {
-    fn is_mp3(&self) -> Option<()>;
-}
-
-impl Mp3 for Path {
-    fn is_mp3(&self) -> Option<()> {
-        let path = self.file_name()?.to_str()?;
-        if path.ends_with(".mp3") {
-            return Some(());
-        } else {
-            return None;
-        }
-    }
-}
-
-fn playlist<R>(root: R) -> std::io::Result<()>
+fn playlist<R>(root: R) -> Result<Vec<String>, io::Error>
 where
     R: AsRef<Path>,
 {
-    let mut playlist = Vec::new();
-    for entry in fs::read_dir(root)? {
-        let entry = entry?.path();
-        let path = entry.clone().as_path();
-        if path.is_mp3().is_some() {
-            playlist.push(path.to_str().unwrap());
-
-            dbg!(path);
+    let mut playlist: Vec<String> = Vec::new();
+    for file in fs::read_dir(root)? {
+        let _entry = file?.path();
+        let entry = _entry.as_path().to_str().unwrap().to_string();
+        if entry.ends_with(".mp3") {
+            playlist.push(entry);
         }
     }
-    dbg!(&playlist);
-    Ok(())
+    Ok(playlist)
 }
 
-//fn play<P>(playlist: P)
-//where
-//    P: AsRef<PathBuf> + std::iter::Iterator,
-//    <P as Iterator>::Item: AsRef<Path>,
-//{
-//    for audio in playlist {
-//        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-//        let sink = rodio::Sink::try_new(&handle).unwrap();
-//
-//        let file = std::fs::File::open(audio).unwrap();
-//        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-//
-//        println!("now playing: ");
-//        sink.sleep_until_end();
-//    }
-//}
+fn play(playlist: Vec<String>) {
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
 
-fn main() -> std::io::Result<()> {
-    let x = Path::new("/Users/ethan/Music/LiquidDNB");
-    let playlist = playlist(x);
+    for audio in playlist.iter() {
+        let file = fs::File::open(audio).unwrap();
+        sink.append(rodio::Decoder::new(io::BufReader::new(file)).unwrap());
+
+        println!("now playing: {audio}");
+        sink.sleep_until_end();
+    }
+}
+
+fn main() -> io::Result<()> {
+    let root = Path::new("/Users/ethan/Music/LiquidDNB");
+    let playlist = playlist(root)?;
+    play(playlist);
+
     Ok(())
 }
