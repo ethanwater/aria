b6b7d2d34a5a1aff2a62e0446cc54a69dcf20a73
ethanwater <angelshatepop@gmail.com>
2023-09-11 15:20:46 -0400

    ability to play albums (sorted) in the playlist

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,79 @@
-use rodio::{Decoder, OutputStream};
-use std::fs::File;
-use std::io::{BufReader, Result};
-use std::path::Path;
+////use rodio::{Decoder, OutputStream};
+//use std::fs::File;
+//use std::io::{BufReader, Result};
+//use std::path::Path;
+//
+//#[allow(unused_variables)]
+//fn main() -> Result<()> {
+//    let root = Path::new("/Users/ethan/Music/liquid/classic_full_cycle.mp3");
+//    let (_stream, handle) = OutputStream::try_default().unwrap();
+//    let file = BufReader::new(File::open(root).unwrap());
+//    let source = Decoder::new(file).unwrap();
+//
+//    Ok(())
+//}
 
-#[allow(unused_variables)]
-fn main() -> Result<()> {
-    let root = Path::new("/Users/ethan/Music/liquid/classic_full_cycle.mp3");
-    let (_stream, handle) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open(root).unwrap());
-    let source = Decoder::new(file).unwrap();
+use rand::prelude::*;
+use rodio::Sink;
+use std::path::Path;
+use std::{fs, io};
+
+fn initialize_playlist<R>(root: R) -> Result<Vec<String>, io::Error>
+where
+    R: AsRef<Path>,
+{
+    let mut playlist: Vec<String> = Vec::new();
+    for file in fs::read_dir(root)? {
+        let _entry = file?.path();
+        let entry = _entry.as_path().to_str().unwrap().to_string();
+        if entry.ends_with(".mp3") {
+            playlist.push(entry);
+        } else if Path::new(&entry).is_dir() {
+            playlist.push(entry);
+        }
+    }
+    Ok(playlist)
+}
+
+fn play_default(playlist: &mut Vec<String>, sink: &Sink) {
+    playlist.sort();
+    for audio in playlist.iter() {
+        let file = fs::File::open(audio).unwrap();
+        sink.append(rodio::Decoder::new(io::BufReader::new(file)).unwrap());
+
+        let audio_name = Path::new(audio).file_name().unwrap().to_str().unwrap();
+        println!("now playing: {audio_name}");
+        sink.sleep_until_end();
+    }
+}
+
+fn play_shuffle(playlist: &mut Vec<String>, sink: &Sink) -> io::Result<()> {
+    let mut rng = rand::thread_rng();
+    playlist.shuffle(&mut rng);
+
+    for audio in playlist.iter() {
+        if Path::new(audio).is_dir() {
+            let mut album = initialize_playlist(Path::new(audio))?;
+            play_default(&mut album, sink);
+        }
+        let file = fs::File::open(audio).unwrap();
+        let audio_name = Path::new(audio).file_name().unwrap().to_str().unwrap();
+        sink.append(rodio::Decoder::new(io::BufReader::new(file)).unwrap());
+        println!("now playing: {audio_name}");
+        sink.sleep_until_end();
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let root = Path::new("/Users/ethan/Music/liquid");
+    let mut playlist = initialize_playlist(root)?;
+
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let _ = play_shuffle(&mut playlist, &sink);
 
     Ok(())
 }
